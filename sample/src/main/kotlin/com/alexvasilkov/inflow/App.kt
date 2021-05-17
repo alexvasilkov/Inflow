51ca1e436b011c02f199b6864f8c34d4e7605905
Alex Vasilkov <alexvasilkov@gmail.com>
2021-05-18 01:08:56 +0700

    Verbose logging in sample

--- sample/src/main/kotlin/com/alexvasilkov/inflow/App.kt
+++ sample/src/main/kotlin/com/alexvasilkov/inflow/App.kt
@@ -3,6 +3,7 @@ package com.alexvasilkov.inflow
 import android.app.Application
 import com.alexvasilkov.inflow.di.initDi
 import inflow.android.InflowAndroid
+import inflow.utils.InflowLogger
 
 class App : Application() {
 
@@ -10,6 +11,7 @@ class App : Application() {
         super.onCreate()
 
         InflowAndroid.init(applicationContext) // TODO: Implement auto loading
+        InflowLogger.verbose = true
 
         initDi()
     }
